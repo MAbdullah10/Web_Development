0706164aab33a87e8bc117515b0d61e1c65025c1
MAbdullah10 <muhammadabdullah100503@gmail.com>
2024-05-22 01:28:49 +0500

    product cards collection implementation through mongoDB

--- Landing_page/routers/homepage.js
+++ Landing_page/routers/homepage.js
@@ -1,16 +1,29 @@
 const express = require("express")
 const router = express.Router()
+const Card = require('../models/card')
 
-router.get('/homepage', (req, res) => {
-    res.render('landingpage') 
-
+router.get('/', async (req, res) => {
+    try{
+        const cards  = await Card.find();
+        const categorizedCards = cards.reduce((acc, card) => {
+            (acc[card.category] = acc[card.category] || []).push(card);
+            return acc;
+         }, {});
+        res.render('landingpage',{categorizedCards})
+    } 
+    catch(err){
+        console.error('Error fetching products:', err);
+        res.status(500).json({message: err.message});
+    }
 })
-router.get('/homepage/contact-us', (req, res) => {
+
+
+router.get('/contact-us', (req, res) => {
     res.render('contact-us') 
-
 })
-router.get("/homepage/order", (req, res) => {
+router.get("/order", (req, res) => {
     res.render('order') 
 })
 
 module.exports = router
+
--- Landing_page/server.js
+++ Landing_page/server.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app=express()
+const mongoose = require("mongoose");
+
+mongoose
+    .connect ("mongodb://127.0.0.1:27017/LuxusLeather",{
+       // useNewUrlParser: true,
+        //useUnifiedTopology: true,
+    })
+    .then(()=>{
+        console.log("connected to DB")
+    })
+    .catch(err=>{
+        console.log("error while connecting to database");
+        console.log(err);
+    }) 
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static('public'));
-
 
 app.listen(4000,()=>{
     console.log('Server is running on port 4000')
@@ -12,11 +25,10 @@ app.listen(4000,()=>{
 
 app.get('/login', (req, res) => {
     res.render('login') 
-
 })
 app.get('/signup', (req, res) => {
     res.render('signup') 
-
 })
+
 const homepageRouter = require ('./routers/homepage')
 app.use('/', homepageRouter)
