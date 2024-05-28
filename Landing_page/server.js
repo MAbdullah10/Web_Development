ae291829d5bb55f8158f7bae78163f7ab537dc86
MAbdullah10 <muhammadabdullah100503@gmail.com>
2024-05-28 14:21:13 +0500

    Login Signup Authentication

--- Landing_page/server.js
+++ Landing_page/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app=express()
 const mongoose = require("mongoose");
 
+const bcrypt = require('bcryptjs');
+
 mongoose
     .connect ("mongodb://127.0.0.1:27017/LuxusLeather",{
        // useNewUrlParser: true,
@@ -19,16 +21,39 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static('public'));
 
-app.listen(4000,()=>{
-    console.log('Server is running on port 4000')
-})
 
-app.get('/login', (req, res) => {
-    res.render('login') 
-})
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(
+    session({
+      key: "Mykey",
+      secret: "This is the secret Key",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        expires: 600000,
+      },
+    })
+    );
+ app.get('/login', (req, res) => {
+        res.render('login',{}) 
+    })
+
 app.get('/signup', (req, res) => {
-    res.render('signup') 
-})
+        res.render('signup') 
+    })
+const authRoutes = require('./routers/auth');
+app.use('/auth', authRoutes);
+
 
 const homepageRouter = require ('./routers/homepage')
 app.use('/', homepageRouter)
+app.listen(4000,()=>{
+    console.log('Server is running on port 4000')
+})
